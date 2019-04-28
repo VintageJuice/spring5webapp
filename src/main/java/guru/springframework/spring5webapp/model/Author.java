321d2da5672be88de5c263835d3bc1209e742e53
patrick <pfoy23@gmail.com>
2019-04-28 19:14:20 +0100

    First commit on test project, first commit using Java Persistence API. Mapped a basic model between authors and books into an in memory database call H2 that is supported by Hibernate. You can render this in a browser on localhost:8080/h2-console - the db has to be jdbc:h2:mem:testdb

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -6,59 +6,75 @@ import java.util.Set;
 
 /**
  * Created by jt on 5/16/17.
+ * Copied by pf on 28/04/19.
  */
 
-public class Author {
-
+@Entity
+public class Author
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
-    private Set<Book> books  = new HashSet<>();
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
 
-    public Author() {
+    public Author()
+    {
     }
 
-    public Author(String firstName, String lastName) {
+    public Author(String firstName, String lastName)
+    {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName, Set<Book> books)
+    {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
     }
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
         this.id = id;
     }
 
-    public String getFirstName() {
+    public String getFirstName()
+    {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName)
+    {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
+    public String getLastName()
+    {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName)
+    {
         this.lastName = lastName;
     }
 
-    public Set<Book> getBooks() {
+    public Set<Book> getBooks()
+    {
         return books;
     }
 
-    public void setBooks(Set<Book> books) {
+    public void setBooks(Set<Book> books)
+    {
         this.books = books;
     }
 }
