ad61b8e70870a77008536fe6743c75c7c57f15b8
patrick <pfoy23@gmail.com>
2019-05-13 18:37:48 +0100

    So just did the first assignment which was to add a Publisher Entity rather than just having a string in the Book Class. I over thought it at first and tried to use Sets to store the data, but all I needed to do was create a simple class and then reference it inside the book with the OneToOne concept tag. Other than that I got everything right which isn't too shabby. I also learned that the order that you save your repo objects in is very important. If they reference each other, they need to exist, and be saved by the repo object, prior to the object that references them.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootStrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootStrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,11 +15,13 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>
 {
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository)
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository)
     {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,18 +32,21 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>
 
     private void initData()
     {
-        initAndSaveAuthorAndBook("Eric", "Evans", "Domain Driven Design", "1234", "Harper Collins");
-        initAndSaveAuthorAndBook("Rob", "Johnson", "J2EE Development Without EJB", "23444", "Worx");
+        initAndSaveAuthorAndBook("Eric", "Evans", "Domain Driven Design", "1234", "Harper Collins", "Harper Collins road");
+        initAndSaveAuthorAndBook("Rob", "Johnson", "J2EE Development Without EJB", "23444", "Worx", "Worx Work Place Road");
     }
 
-    private void initAndSaveAuthorAndBook(String firstName, String lastName, String title, String isbn, String publisher)
+    private void initAndSaveAuthorAndBook(String firstName, String lastName, String title, String isbn, String publisherName, String publisherAddress)
     {
         Author author = new Author(firstName, lastName);
+        Publisher publisher = new Publisher(publisherName, publisherAddress);
         Book book = new Book(title, isbn, publisher);
+
         author.getBooks().add(book);
         book.getAuthors().add(author);
 
         authorRepository.save(author);
+        publisherRepository.save(publisher); //order of saves is important!
         bookRepository.save(book);
     }
 }
